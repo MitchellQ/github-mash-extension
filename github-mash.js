d2e5faaea14993dd67ef895ede3b1891fb3fc490
Mitchell Quarrie <40969453+MitchellQ@users.noreply.github.com>
2025-02-11 19:06:42 +1300

    frmt

--- github-mash.js
+++ github-mash.js
@@ -9,80 +9,84 @@
 // ==/UserScript==
 
 (function () {
-    'use strict';
+  'use strict';
 
-    console.log('GitMash loaded');
+  console.log('GitMash loaded');
 
-    // Add custom CSS to style the buttons and summary element
-    const style = document.createElement('style');
-    style.textContent = `
+  // Add custom CSS to style the buttons and summary element
+  const style = document.createElement('style');
+  style.textContent = `
         .merge-box-button:enabled,
         .js-merge-method-menu-button {
             background-color: #6A0DAD !important; /* Purple */
         }
     `;
-    document.head.appendChild(style);
+  document.head.appendChild(style);
 
-    // Handle the SPA nature of github, listen for navigation changes and act on those.
-    window.navigation.addEventListener("navigate", (event) => {
-        if (event.navigationType === 'replace') {
-            gitMash();
-        }
-    });
+  // Handle the SPA nature of github, listen for navigation changes and act on those.
+  window.navigation.addEventListener('navigate', (event) => {
+    if (event.navigationType === 'replace') {
+      gitMash();
+    }
+  });
 })();
 
 let observer;
 
 function gitMash() {
-    if (observer) {
+  console.log('0.2.8');
+  if (observer) {
+    observer.disconnect();
+    observer = undefined;
+    console.log('GitMash not listening...');
+  }
+
+  if (window.location.href.match('https://github.com/.*?/pull/.*') == null) {
+    return;
+  }
+
+  console.log(window.location.href);
+
+  const developBranch = 'develop';
+  const featureBranchPrefix = 'feature/';
+
+  const baseBranch = document.querySelector('.base-ref').textContent;
+  const headBranch = document.querySelector('.head-ref').textContent;
+
+  if (!baseBranch || !headBranch) {
+    return;
+  }
+
+  let selector;
+  if (
+    baseBranch === developBranch &&
+    headBranch.startsWith(featureBranchPrefix)
+  ) {
+    selector = '.js-merge-box-button-squash';
+  } else {
+    selector = '.js-merge-box-button-merge';
+  }
+
+  let element = document.querySelector(selector);
+
+  if (element) {
+    selectGitMash(element);
+  } else {
+    observer = new MutationObserver((_) => {
+      let element = document.querySelector(selector);
+      if (element) {
         observer.disconnect();
         observer = undefined;
-        console.log('GitMash not listening...');
-    }
-
-    if (window.location.href.match('https:\/\/github.com\/.*?\/pull\/.*') == null) {
-        return;
-    }
-
-    console.log(window.location.href);
-
-    const developBranch = 'develop';
-    const featureBranchPrefix = 'feature/';
-
-    const baseBranch = document.querySelector('.base-ref').textContent;
-    const headBranch = document.querySelector('.head-ref').textContent;
-
-    if (!baseBranch || !headBranch) {
-        return;
-    }
-
-    let selector;
-    if (baseBranch === developBranch && headBranch.startsWith(featureBranchPrefix)) {
-        selector = '.js-merge-box-button-squash';
-    } else {
-        selector = '.js-merge-box-button-merge';
-    }
-
-    let element = document.querySelector(selector);
-
-    if (element) {
         selectGitMash(element);
-    } else {
-        observer = new MutationObserver(_ => {
-            let element = document.querySelector(selector);
-            if (element) {
-                observer.disconnect();
-                observer = undefined;
-                selectGitMash(element);
-                console.log('GitMash not listening...')
-            }
-        });
-        console.log('GitMash listening...');
-        observer.observe(document.body, { childList: true, subtree: true });
-    }
+        console.log('GitMash not listening...');
+      }
+    });
+    console.log('GitMash listening...');
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
 }
 
 function selectGitMash(element) {
-    element.click();
-    console.log(element.textContent + ' selected!');
+  element.click();
+  console.log(element.textContent + ' selected!');
 }
